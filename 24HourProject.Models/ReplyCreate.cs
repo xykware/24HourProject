fb959126c83d70ea90224e70e1b8f8bea2c892e9
Ethan Richardson <ethan.taylor.richardson@gmail.com>
2021-10-20 11:22:36 -0400

    added reply stuff

---
+++ 24HourProject.Models/ReplyCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourProject.Models
+{
+    public class ReplyCreate
+    {
+        [Required]
+        public int CommentId { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+    }
+}
---
+++ 24HourProject.Services/ReplyService.cs
@@ -0,0 +1,37 @@
+﻿using _24HourProject.Data;
+using _24HourProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourProject.Services
+{
+    public class ReplyService
+    {
+        private readonly Guid _userId;
+
+        public ReplyService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateReply(ReplyCreate model)
+        {
+            var entity =
+                new Reply()
+                {
+                    AuthorId = _userId,
+                    Text = model.Text,
+                    CommentId = model.CommentId
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Replies.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
