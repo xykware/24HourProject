9090e4d8fba55b3dabb975ea22dd6670c1cf73e2
Joe.Micucci <micucci.joseph@gmail.com>
2021-10-20 12:10:44 -0400

    comments setup

---
+++ 24HourProject.Models/CommentCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourProject.Models
+{
+    public class CommentCreate
+    {
+        public int PostId { get; set; }
+        [Required]
+        [MinLength (1, ErrorMessage = "Comment cannot be blank.")]
+        [MaxLength (8000, ErrorMessage = "Maximum characters used. Please shorten your comment.")]
+        public string Content { get; set; }
+    }
+}
---
+++ 24HourProject.Models/CommentDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourProject.Models
+{
+    public class CommentDetail
+    {
+        public int PostId { get; set; }
+        public string Content { get; set; }
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+        [Display(Name = "Modified")]
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
